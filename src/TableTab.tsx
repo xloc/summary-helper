7e9654e7d3a6d128f08313464a6ef3ac50b3ef14
xloc <xloc.cc@outlook.com>
2022-03-17 02:21:41 -0700

    change styles

--- src/TableTab.tsx
+++ src/TableTab.tsx
@@ -1,5 +1,6 @@
-import React, { FormEventHandler, useState } from 'react';
-import { SummaryInfo, UnitInfo } from './logic';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { SummaryInfo } from './logic';
 
 
 
@@ -14,16 +15,19 @@ interface EditableTDProps {
 function TDEdit({ className, text, i_unit, i_model, attribute, onChange }: EditableTDProps) {
   const [value, setValue] = useState(text);
   return (
-    <td {...{ className }}><input
-      type="text"
-      value={value}
-      onChange={(e) => {
-        const ee = e as React.ChangeEvent<HTMLInputElement>;
-        setValue(ee.target.value);
-      }}
-      onBlur={() => {
-        if (value) onChange(i_model, i_unit, attribute, value);
-      }} /></td>
+    <td {...{ className }}>
+      <input
+        className='w-full opacity-80'
+        type="text"
+        value={value}
+        onChange={(e) => {
+          const ee = e as React.ChangeEvent<HTMLInputElement>;
+          setValue(ee.target.value);
+        }}
+        onBlur={() => {
+          if (value) onChange(i_model, i_unit, attribute, value);
+        }} />
+    </td>
   );
 }
 
@@ -35,30 +39,32 @@ export default function TableTab({ data, onChange }: TableTabProps) {
   if (!data) return (<div>Invalid Data</div>);
 
   return (
-    <table className='text-sm font-mono w-full'>
-      <thead className='font-bold'>
-        <tr><td>Year: {data.year}  Week: {data.week}</td></tr>
-        <tr>
-          <td>Serial Number</td>
-          <td>Ticket</td>
-          <td>Country</td>
-          <td>Summary</td>
-          <td>Company</td>
-        </tr>
-      </thead>
-      <tbody>
-        {(data.models.flatMap((model, i_model) => model.units.map((unit, i_unit) => {
-          return (
-            <tr key={`${i_model} ${i_unit}`} className="">
-              <td className='select-all'>{unit.serialNumber}</td>
-              <td className='select-all'>{unit.careTicketNumber}</td>
-              <TDEdit className='select-all' {...{ i_model, i_unit, attribute: 'country', onChange, text: unit.country }} />
-              <td>{unit.summary}</td>
-              <td>{unit.company}</td>
-            </tr>
-          );
-        })))}
-      </tbody>
-    </table>
+    <div className="h-full w-full">
+      <h1 className='font-mono text-sm font-bold py-3'>Year: {data.year}  Week: {data.week}</h1>
+      <table className='text-sm font-mono w-full'>
+        <thead className='font-bold'>
+          <tr>
+            <td>Serial Number</td>
+            <td>Ticket</td>
+            <td>Country</td>
+            <td>Summary</td>
+            <td>Company</td>
+          </tr>
+        </thead>
+        <tbody>
+          {(data.models.flatMap((model, i_model) => model.units.map((unit, i_unit) => {
+            return (
+              <tr key={`${i_model} ${i_unit}`} className="hover:bg-slate-200">
+                <td className='select-all w-[12em]'>{unit.serialNumber}</td>
+                <td className='select-all w-[6em]'>{unit.careTicketNumber}</td>
+                <TDEdit className='select-all w-[12em] pr-2' {...{ i_model, i_unit, attribute: 'country', onChange, text: unit.country }} />
+                <TDEdit className='w-[35em] pr-2' {...{ i_model, i_unit, attribute: 'summary', onChange, text: unit.summary }} />
+                <td>{unit.company}</td>
+              </tr>
+            );
+          })))}
+        </tbody>
+      </table>
+    </div>
   );
 }
